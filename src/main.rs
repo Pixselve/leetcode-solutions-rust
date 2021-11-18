323bbee869356f226913596a02e943438e3f0d92
Pixselve <38262536+Pixselve@users.noreply.github.com>
2021-11-18 17:32:18 +0100

    :sparkles: 20. Valid Parentheses

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ mod longest_substring_without_repeating_characters;
 mod minimum_number_of_moves_to_seat_everyone;
 mod roman_to_integer;
 mod longest_common_prefix;
+mod valid_parentheses;
+mod palindrome_number;
 
 fn main() {
 
---
+++ src/valid_parentheses.rs
@@ -0,0 +1,39 @@
+
+struct Solution {}
+
+impl Solution {
+    pub fn is_valid(s: String) -> bool {
+        let mut parentheses_stack: Vec<u8> = vec![];
+
+
+
+        for character in s.chars() {
+            match character {
+                '{' => {parentheses_stack.push(0)}
+                '(' => {parentheses_stack.push(1)}
+                '[' => {parentheses_stack.push(2)}
+
+                '}' => {if parentheses_stack.last().unwrap_or(&3).eq(&0) {parentheses_stack.pop();} else {return false}}
+                ')' => {if parentheses_stack.last().unwrap_or(&3).eq(&1) {parentheses_stack.pop();} else {return false}}
+                ']' => {if parentheses_stack.last().unwrap_or(&3).eq(&2) {parentheses_stack.pop();} else {return false}}
+                _ => {}
+            }
+        }
+        parentheses_stack.is_empty()
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn tests() {
+        let input_and_solutions = vec![("()", true), ("()[]{}", true), ("(]", false), ("([)]", false), ("{[]}", true)];
+        for input in input_and_solutions {
+            assert_eq!(Solution::is_valid(input.0.parse().unwrap()), input.1, "we are testing addition with {}", input.0)
+        }
+    }
+}
